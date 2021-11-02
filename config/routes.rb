12817e4f4460a354f389ddacd190e5e2a630c5d1
Mirian Zhagui <mirianzhagui@Mirians-Air.lan>
2021-11-02 12:12:05 -0400

    review and post table

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :reviews
+  resources :posts
   post '/auth/login', to: 'authentications#login'
   get '/auth/verify', to: 'authentications#verify'
   resources :users
---
+++ db/migrate/20211102160726_create_posts.rb
@@ -0,0 +1,13 @@
+class CreatePosts < ActiveRecord::Migration[6.1]
+  def change
+    create_table :posts do |t|
+      t.string :title
+      t.string :address
+      t.string :food_description
+      t.string :img_url
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
