2780a59a5fc2aef95db9bd83ec543cae68bbe7c0
Mirian Zhagui <mirianzhagui@Mirians-Air.lan>
2021-11-02 12:00:23 -0400

    backend auth

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,9 +18,9 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     
     if @user.save
-      @token = encode({id: @user.id})
+      @token = encode({ id: @user.id })
       render json: {
-        user: @user.attributes.except("password_digest"),
+        user: @user.attributes.except('password_digest'),
         token: @token
         }, status: :created
     else
